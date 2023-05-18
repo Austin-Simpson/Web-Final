62c839eba74b75595182997d86d25c7fa33a0616
Brian Bak <bbak@chapman.edu>
2023-05-18 11:38:32 -0700

    Update _app.js
    slightly changed the layout of the page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import styled from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   html, body {
@@ -20,15 +21,57 @@ const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
+
+const Wrapper = styled.div`
+  // background-color: ${props => props.theme.colors.primary};
+  // padding: 20px;
+  // color: white;
+  // display: flex;
+  // flex-direction: column;
+  // align-items: center;
+
+  background-color: #0070f3; /* Set the background color to blue */
+  min-height: 100vh; /* Ensure the wrapper occupies the full viewport height */
+  padding: 20px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+  // display: flex;
+  // justify-content: center; 
+  // align-items: center;
+  // height: 30px; 
+`;
+
+
+
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <>
+//       <GlobalStyle />
+//       <ThemeProvider theme={theme}>
+//         <Component {...pageProps} />
+//       </ThemeProvider>
+//       </>
+//   )
+// }
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Wrapper>
+          <Title>AB's Restaurant Finder</Title>
+          <Component {...pageProps} />
+        </Wrapper>
       </ThemeProvider>
-      </>
-  )
+    </>
+  );
 }
